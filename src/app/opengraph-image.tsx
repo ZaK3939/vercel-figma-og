4c591002072f8fbe741f8bfc373ff08263ae8cb8
ZaK3939 <tsu.aria.kalei@gmail.com>
2024-06-06 00:49:07 +0400

    feat: update

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -1,31 +1,30 @@
-import { experimental_FigmaImageResponse } from "next/og";
+import { experimental_FigmaImageResponse } from 'next/og';
 
-export const runtime = "edge";
+export const runtime = 'edge';
 
-export const alt = "Figma to OG Image";
+export const alt = 'Figma to OG Image';
 
-export const contentType = "image/png";
+export const contentType = 'image/png';
 export const revalidate = 3600;
 
 export default async function Image() {
-  const geistSemiBold = await fetch(
-    new URL("./Geist-SemiBold.otf", import.meta.url)
-  ).then((res) => res.arrayBuffer());
+  const geistSemiBold = await fetch(new URL('./Geist-SemiBold.otf', import.meta.url)).then((res) => res.arrayBuffer());
 
   return experimental_FigmaImageResponse({
-    url: "https://www.figma.com/file/lbTOxOq2wdE4ZK5vH4ADvb/Changelog-Images?type=design&node-id=3470-8964&mode=design&t=8r7sUnhzATSbyOhL-4",
+    // url: "https://www.figma.com/file/lbTOxOq2wdE4ZK5vH4ADvb/Changelog-Images?type=design&node-id=3470-8964&mode=design&t=8r7sUnhzATSbyOhL-4",
+    url: 'https://www.figma.com/design/hPCU9QkipbHm6h1kyMIH3W/FigmaImageResponse-Demo-(Copy)?node-id=1-60&m=dev',
     template: {
-      Title: "A dynamic OG Image",
+      Title: 'A dynamic OG Image',
       Description: {
-        value: "Created using a few lines of code",
+        value: 'Created using a few lines of code',
         props: { centerHorizontally: true },
       },
     },
     fonts: [
       {
-        name: "Geist",
+        name: 'Geist',
         data: geistSemiBold,
-        style: "normal",
+        style: 'normal',
         weight: 600,
       },
     ],
