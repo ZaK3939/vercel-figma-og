4ed64fc79d28730fb3fe08c125134d1fb0df0fec
ZaK3939 <tsu.aria.kalei@gmail.com>
2024-06-06 00:45:32 +0400

    feat: update

--- src/app/image/route.tsx
+++ src/app/image/route.tsx
@@ -1,21 +1,20 @@
-import { experimental_FigmaImageResponse } from "next/og";
+import { experimental_FigmaImageResponse } from 'next/og';
 
-export const runtime = "edge";
+export const runtime = 'edge';
 
 export async function GET(request: Request) {
   const [geistSemiBoldFont] = await Promise.all([
-    fetch(new URL("../Geist-SemiBold.otf", import.meta.url)).then((res) =>
-      res.arrayBuffer()
-    ),
+    fetch(new URL('../Geist-SemiBold.otf', import.meta.url)).then((res) => res.arrayBuffer()),
   ]);
 
   const { searchParams } = new URL(request.url);
-  const title = searchParams.get("title") ?? "A dynamic OG Image";
-  const description =
-    searchParams.get("description") ?? "Created using a few lines of code";
+  const title = searchParams.get('title') ?? 'A dynamic OG Image';
+  const description = searchParams.get('description') ?? 'Created using a few lines of code';
 
   return experimental_FigmaImageResponse({
-    url: "https://www.figma.com/file/lbTOxOq2wdE4ZK5vH4ADvb/Changelog-Images?type=design&node-id=3470-8964&mode=design&t=8r7sUnhzATSbyOhL-4",
+    // url: "https://www.figma.com/file/lbTOxOq2wdE4ZK5vH4ADvb/Changelog-Images?type=design&node-id=3470-8964&mode=design&t=8r7sUnhzATSbyOhL-4",
+    // href="https://www.figma.com/file/muVsFH50Ddm45BGtFKEdG5/FigmaImageResponse-Demo?type=design&node-id=1-60&mode=design&t=OPmzvOPfCgO0cLSp-0"
+    url: 'https://www.figma.com/design/hPCU9QkipbHm6h1kyMIH3W/FigmaImageResponse-Demo-(Copy)?node-id=1-60&m=dev',
     template: {
       Title: {
         value: title,
@@ -28,9 +27,9 @@ export async function GET(request: Request) {
     },
     fonts: [
       {
-        name: "Geist",
+        name: 'Geist',
         data: geistSemiBoldFont,
-        style: "normal",
+        style: 'normal',
         weight: 600,
       },
     ],
